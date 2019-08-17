b8bd3e4fc495b4a30f0c6580e8b8d6a78a7258c5
kdzumba <knowledgedzumba@gmail.com>
2019-08-17 15:45:18 +0200

    Added navigation bar to site

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 Bootstrap(app)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
