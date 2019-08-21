0d7bc6469c98783e229250116f4eeffadb7348d7
Zanele01 <0706885n@students.wits.ac.za>
2019-08-22 00:05:16 +0200

    Sentiment analysis web application

--- app.py
+++ app.py
@@ -19,8 +19,46 @@ def __repr__(self):
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
 
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your text'
+
+        pass
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a probelm deleting your text'
+
+app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your text'
+    else:
+        return render_template('index.html', task=task)
 
 if __name__ == '__main__':
     app.run(debug=True)
