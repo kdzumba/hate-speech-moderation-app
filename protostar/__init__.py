4a16d1b6b09b3952ad96e32d8ad44b2abcc865da
kdzumba <knowledgedzumba@gmail.com>
2019-09-06 18:43:27 +0200

    final version before submission

--- protostar/__init__.py
+++ protostar/__init__.py
@@ -3,11 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "a6593d00cb970d5b6b421473918506be"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY TRACK MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from protostar import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
